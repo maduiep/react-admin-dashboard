e99976b177e9a1081e67abd6560342be30076d69
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-09-06 15:26:47 +0100

    completed sidebar contents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './sass/main.scss'
 import Topbar from './components/layouts/Topbar'
+import Sidebar from './components/layouts/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar/>
+        <div className="others">others</div>
+      </div>
+
     </div>
   );
 }
--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -1,9 +1,94 @@
 import React from 'react'
+import {LineStyle, 
+     Timeline, 
+     TrendingUp, 
+     PermIdentity,
+     AttachMoney,
+     MailOutline, 
+     Storefront, 
+     BarChart, 
+     DynamicFeed, 
+     WorkOutline,
+     ChatBubbleOutline, 
+     Report} from '@material-ui/icons'
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <div className="sidebar__wrapper">
+                <div className="sidebar__menu">
+                    <h3 className="sidebar__title">Dashboard</h3>
+                    <ul className="sidebar__list">
+                        <li className="item active">
+                            <LineStyle className="item__icon"/>
+                            Home
+                        </li>
+                        <li className="item">
+                            <Timeline className="item__icon"/>
+                            Anaytics
+                        </li>
+                        <li className="item">
+                            <TrendingUp className="item__icon"/>
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebar__menu">
+                    <h3 className="sidebar__title">Quick Menu</h3>
+                    <ul className="sidebar__list">
+                        <li className="item">
+                            <PermIdentity className="item__icon"/>
+                            Users
+                        </li>
+                        <li className="item">
+                            <Storefront className="item__icon"/>
+                            Products
+                        </li>
+                        <li className="item">
+                            <AttachMoney className="item__icon"/>
+                            Transactions
+                        </li>
+                        <li className="item">
+                            <BarChart className="item__icon"/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebar__menu">
+                    <h3 className="sidebar__title">Notifications</h3>
+                    <ul className="sidebar__list">
+                        <li className="item">
+                            <MailOutline className="item__icon"/>
+                            Mail
+                        </li>
+                        <li className="item">
+                            <DynamicFeed className="item__icon"/>
+                            Feedback
+                        </li>
+                        <li className="item">
+                            <ChatBubbleOutline className="item__icon"/>
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebar__menu">
+                    <h3 className="sidebar__title">Staff</h3>
+                    <ul className="sidebar__list">
+                        <li className="item">
+                            <WorkOutline className="item__icon"/>
+                            Manage
+                        </li>
+                        <li className="item">
+                            <Timeline className="item__icon"/>
+                            Anaytics
+                        </li>
+                        <li className="item">
+                            <Report className="item__icon"/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
