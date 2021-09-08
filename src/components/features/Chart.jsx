9767a2aec1408353f26fc017901c23aac513032e
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2021-09-08 15:11:19 +0100

    added chart features in home page

---
+++ src/components/features/Chart.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
+function Chart({title, data, dataKey, grid}) {
+    return (
+        <div className="chart">
+            <h3 className="chart__title">{title}</h3>
+            <ResponsiveContainer width="100%" aspect={4/1}>
+                <LineChart data={data}>
+                    <XAxis dataKey ="name" stroke="#5550bd"/>
+                    <Line type="monotone" dataKey={dataKey} stroke="#5550bd"/>
+                    <Tooltip />
+                    {grid && <CartesianGrid stoke="#e0dfdf" 
+                    strokeDasharray="5 5"/>}
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+
+export default Chart
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import FeaturedInfo from '../features/FeaturedInfo'
+import Chart from '../features/Chart'
+import { userData } from '../../commonData'
 
 function Home() {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
         </div>
     )
 }
